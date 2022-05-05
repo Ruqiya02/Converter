0c1bf170a51cd1d41cf71b6f91809c94cc089478
Heyderli Ruqiyye <heyderliruqiyye2@gmail.com>
2022-05-05 11:28:47 +0400

    first change

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-const 
+const input=document.querySelector("input");
+const buttons=document.querySelectorAll(".selection")
+input.addEventListener("keyup",(event)=>{
+    if(event.keyCode>65&&event.keyCode<90){
+        input.innerHTML=""
+    }
+})
+// fetch("https://api.exchangerate.host/latest?base=USD&symbols=RUB ")
+// .then((res)=>{
+//     return res.json()
+// })
+// .then((data)=>{
+//     console.log(data.base)
+// })
+
+function api1(){
+    fetch("https://api.exchangerate.host/latest?base=USD&symbols=RUB ")
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data.rates[Object.keys(data.rates)[0]])
+    })
+ 
+
+}
+api1()
+
+
